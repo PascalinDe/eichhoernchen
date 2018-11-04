61ba108f484247fc8c95ed41f776b016ac8c0473
Carine Dengler <git@carine-dengler.de>
2018-11-04 21:05:47 +0100

    added SQLite table manipulation

--- src/sqlite.py
+++ src/sqlite.py
@@ -21,6 +21,7 @@
 
 
 # standard library imports
+import sqlite3
 import collections
 
 # third party imports
@@ -28,7 +29,7 @@ import collections
 
 
 _Task = collections.namedtuple(
-    "Task", ["name", "start_time", "end_time", "priority"]
+    "Task", ["name", "start", "end", "total", "due"]
 )
 
 
@@ -36,10 +37,78 @@ class Task(_Task):      # pylint: disable=missing-docstring
     __slots__ = ()
 
     def __repr__(self):
-        repr_ = "{name} ({start_time} - {end_time}): {priority}".format(
+        repr_ = "{name} ({start} - {end}, total: {total}) (due: {due})".format(
             name=self.name,
-            start_time=self.start_time,
-            end_time=self.end_time,
-            priority=self.priority
+            start=self.start,
+            end=self.end,
+            total=self.total,
+            due=self.due
         )
         return repr_
+
+
+class SQLite():
+    """Eichhörnchen SQLite database interface.
+
+    :cvar str DATABASE: SQLite database
+    :cvar str TABLE: SQLite table
+    :cvar dict COLUMN_DEFS: SQLite column definitions
+    """
+
+    DATABASE = "eichhoernchen"
+    TABLE = "tasks"
+    COLUMN_DEFS = {
+        "name": "TEXT PRIMARY KEY",
+        "start": "TIMESTAMP",
+        "end": "TIMESTAMP",
+        "total": "TIMESTAMP",
+        "due": "TIMESTAMP",
+    }
+
+    def __init__(self):
+        """Initialize SQLite3 database interface."""
+        return
+
+    def connect(self):
+        """Connect to Eichhörnchen SQLite3 database.
+
+        :return: connection
+        :rtype: Connect
+        """
+        connection = sqlite3.connect(
+            self.DATABASE,
+            detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES
+        )
+        connection.row_factory = sqlite3.Row
+        return connection
+
+    def create_table(self):
+        """Create tasks table."""
+        connection = self.connect()
+        sql = "CREATE TABLE IF NOT EXISTS {table} ({column_defs})".format(
+            table=self.TABLE,
+            column_defs=", ".join(
+                "{} {}".format(k, v) for k, v in self.COLUMN_DEFS.items()
+            )
+        )
+        connection.execute(sql)
+        connection.commit()
+        connection.close()
+        return
+
+    def insert(self, rows):
+        """Insert tasks.
+
+        :param list rows: rows
+        """
+        connection = self.connect()
+        sql = "INSERT INTO {table} VALUES ({columns})".format(
+            table=self.TABLE,
+            columns=", ".join(
+                "?" for _ in self.COLUMN_DEFS.items()
+            )
+        )
+        connection.executemany(sql, rows)
+        connection.commit()
+        connection.close()
+        return
