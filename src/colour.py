46f5632c43e7fd711d1e108a19de678f9aa58b08
Carine Dengler <git@carine-dengler.de>
2019-08-25 10:41:09 +0200

    moved colours to separate module

---
+++ src/colour.py
@@ -0,0 +1,82 @@
+#    This file is part of Eichhörnchen 1.1.
+#    Copyright (C) 2019  Carine Dengler
+#
+#    Eichhörnchen is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+:synopsis: Colour schemes.
+"""
+
+
+# standard library imports
+# third party imports
+# library specific imports
+
+
+class FGColours():
+    """
+    Foreground colours.
+
+    Black       0;30     Dark Gray     1;30
+    Blue        0;34     Light Blue    1;34
+    Green       0;32     Light Green   1;32
+    Cyan        0;36     Light Cyan    1;36
+    Red         0;31     Light Red     1;31
+    Purple      0;35     Light Purple  1;35
+    Brown       0;33     Yellow        1;33
+    Light Gray  0;37     White         1;37
+
+    .. _Source: http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/x329.html
+    """
+    BLACK = "\033[0;31m"
+    BLUE = "\033[0;34m"
+    GREEN = "\033[0;32m"
+    CYAN = "\033[0;36m"
+    RED = "\033[0;31m"
+    PURPLE = "\033[0;35m"
+    BROWN = "\033[0;33m"
+    LIGHT_GRAY = "\033[0;37m"
+    DARK_GRAY = "\033[1;30m"
+    LIGHT_BLUE = "\033[1;34m"
+    LIGHT_GREEN = "\033[1;32m"
+    LIGHT_CYAN = "\033[1;36m"
+    LIGHT_RED = "\033[1;31m"
+    LIGHT_PURPLE = "\033[1;35m"
+    YELLOW = "\033[1;33m"
+    WHITE = "\033[1;37m"
+
+
+class BGColours():
+    """Background colours.
+
+    Black       40
+    Blue        44
+    Green       42
+    Cyan        46
+    Red         41
+    Purple      45
+    Brown       43
+    Light Gray  47
+
+    .. _Source: http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/x329.html
+    """
+    BLACK = "\033[41m"
+    BLUE = "\033[44m"
+    GREEN = "\033[42m"
+    CYAN = "\033[46m"
+    RED = "\033[41m"
+    PURPLE = "\033[45m"
+    BROWN = "\033[43m"
+    LIGHT_GRAY = "\033[47m"
