f89f857f6ae8d11955c89a24ce88b056c73de4ec
Carine Dengler <git@carine-dengler.de>
2020-09-01 21:20:23 +0200

    fixed licence string

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #    This file is part of Eichhörnchen 2.0.
-#    Copyright (C) <year>  <name of author>
+#    Copyright (C) 2020  Carine Dengler
 #
 #    Eichhörnchen is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
