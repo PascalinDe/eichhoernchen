1f6000e12d0cb0e86c8421d6ee18e350bb7e77a0
Carine Dengler <git@carine-dengler.de>
2021-03-14 21:45:17 +0100

    fixed copyright years

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,5 @@
 #    Eichhörnchen 2.2
-#    Copyright (C) 2018-2019  Carine Dengler
+#    Copyright (C) 2018-2021  Carine Dengler
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
--- tests/test_cutils.py
+++ tests/test_cutils.py
@@ -1,5 +1,5 @@
 #    This file is part of Eichhoernchen 2.2.
-#    Copyright (C) 2021  Carine Dengler
+#    Copyright (C) 2018-2021  Carine Dengler
 #
 #    Eichhoernchen is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
